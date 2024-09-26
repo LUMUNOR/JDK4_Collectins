67e456d9173becd2f3a1bda375f1444fa995e4ed
lumunor <lukik884@gmail.com>
2024-09-26 16:19:02 +0300

    init

---
+++ src/main/java/Directory/EmployDirectory.java
@@ -0,0 +1,4 @@
+package Directory;
+
+public class EmployDirectory {
+}
---
+++ src/main/java/Employ/Employ.java
@@ -0,0 +1,16 @@
+package Employ;
+
+public class Employ {
+    private int tabNumber;
+    private int phoneNumber;
+    private String name;
+    private int experience;
+
+    public Employ(int tabNumber, int phoneNumber, String name, int experience){
+        this.tabNumber=tabNumber;
+        this.phoneNumber=phoneNumber;
+        this.name=name;
+        this.experience=experience;
+    }
+
+}
